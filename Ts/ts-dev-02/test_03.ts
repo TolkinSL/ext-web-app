b6b0602895beeb090237d2d1b94b02d045eebcb1
TolkinSL <tolkinsl@yandex.ru>
2024-03-31 10:41:04 +0300

    Dev-01 TS never null

---
+++ Ts/ts-dev-02/test_03.ts
@@ -0,0 +1,13 @@
+const id03: symbol = Symbol('id');
+const data03 = {
+  [id03]: 1,
+};
+
+console.log(data03[id03]);
+
+const id04: string = 'id';
+const data04 = {
+  [id04]: 1,
+};
+
+console.log(data04[id04]);
